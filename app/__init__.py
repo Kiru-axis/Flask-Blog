48186df43130d240d6da89871f704fa8172ef417
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-06-13 17:12:30 +0300

    adding and initalising bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,15 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
+
+bootstrap = Bootstrap()
 
 # Initializing application
 app = Flask(__name__)
+
+# Initializing Flask bootstrap Extension
+bootstrap = Bootstrap(app)
+
+
+
+from app import views
