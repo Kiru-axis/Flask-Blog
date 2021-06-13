844257abb1e47bf12c97dfcfe42099d4c64e64e9
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-06-13 20:40:53 +0300

    cistom validators

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
-
+from wtforms import ValidationError
 # registration form
 class RegistrationForm(FlaskForm):
     email = StringField('Email Address', validators=[Required(), Email()])
@@ -10,3 +10,14 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators = [Required(), EqualTo('password_confirm', message = 'Passwords must match')])
     password_confirm = PasswordField('Confirm Password', validators =[Required()])
     submit = SubmitField('Sign Up')
+# custom validators
+    # validate username
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
+
+
+    # validate email
+    def validate_email(self,data_field):
+            if User.query.filter_by(email =data_field.data).first():
+                raise ValidationError('There is an account with that email')
