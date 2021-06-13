723f0ec7d247e1ff369886a3cafbf8b318af3208
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-06-13 20:52:51 +0300

    completed registration route

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
+from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 from wtforms import ValidationError
@@ -21,3 +21,6 @@ class RegistrationForm(FlaskForm):
     def validate_email(self,data_field):
             if User.query.filter_by(email =data_field.data).first():
                 raise ValidationError('There is an account with that email')
+
+
+# Login forms
