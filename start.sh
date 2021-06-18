61149ce712dd355a350d7c6840f8b99feb117455
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-06-18 16:06:49 +0300

    erasing the script file

--- start.sh
+++
@@ -1,6 +0,0 @@
-export SECRET_KEY='7cb73601ae84f29562bb8a5228040a'
-# secrets lol
-export MAIL_USERNAME=peter.kiru@student.moringaschool.com
-export MAIL_PASSWORD=Garang.Kiru
-
-python3.8 manage.py server
